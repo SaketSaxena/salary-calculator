9e6b9f7112eff58f528b73cea1cf3b59c49bb709
SaketSaxena <saket.saxena@posten.no>
2021-02-10 15:29:26 +0100

    InitialCommit

---
+++ src/main/java/TaxCalculator.java
@@ -0,0 +1,23 @@
+public class TaxCalculator {
+
+    public long calculateTax(long income) {
+
+        if(income <= 0) {
+            throw new IllegalArgumentException("Income should be greater than 0");
+        }
+
+        if(income > 250000 && income <= 500000) {
+            return (long)((income - 250000)*0.05);
+        }
+
+        if(income > 500000 && income <= 1000000) {
+            return (long)((income - 500000)*0.20) + 12500;
+        }
+
+        if(income > 1000000) {
+            return (long)((income - 1000000)*0.30) + 112500;
+        }
+
+        return 0;
+    }
+}
---
+++ src/test/java/TaxCalculatorTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TaxCalculatorTest {
+
+    private TaxCalculator taxCalculator;
+
+    @BeforeEach
+    void setUp() {
+        taxCalculator = new TaxCalculator();
+    }
+
+    @Test
+    void should_calculate_tax_when_income_is_below_250000() {
+        long tax = taxCalculator.calculateTax(200000);
+
+        assertEquals(0, tax);
+    }
+
+    @Test
+    void should_calculate_tax_when_income_is_between_250001_and_500000() {
+        long tax = taxCalculator.calculateTax(400000);
+
+        assertEquals(7500, tax);
+    }
+
+    @Test
+    void should_calculate_tax_when_income_is_between_500001_and_1000000() {
+        long tax = taxCalculator.calculateTax(900000);
+
+        assertEquals(92500, tax);
+    }
+
+    @Test
+    void should_calculate_tax_when_income_is_above_1000000() {
+        long tax = taxCalculator.calculateTax(1200000);
+
+        assertEquals(172500, tax);
+    }
+
+    @Test
+    void should_throw_illegal_argument_exception_if_income_is_less_than_1() {
+        assertThrows(IllegalArgumentException.class, () -> taxCalculator.calculateTax(-2000));
+    }
+}
